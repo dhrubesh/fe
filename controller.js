a9feddc36b49b2d2dfe0a53e17fab08f9f1ddbb0
Dhrubesh <dhrubesh@trycinema.com>
2017-02-27 18:24:37 +0530

    http

--- controller.js
+++ controller.js
@@ -1,6 +1,6 @@
  var app = angular.module('mainApp',[]);
  app.controller('board',function($scope,$http){
- 		$http.get('/companies.json')
+ 		$http.get('https://raw.githubusercontent.com/dhrubesh/fe/master/companies.json')
  		.success(function(response){
  			$scope.companies=response.Company;
  		});
